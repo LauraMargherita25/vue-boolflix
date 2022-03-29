cebaa8758dda525fe9f3533efa4ebf3daaba9da2
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-29 11:36:41 +0200

    Base structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <file-header />
+    <file-main />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FileHeader from './components/FileHeader.vue'
+import FileMain from './components/FileMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FileHeader,
+    FileMain
   }
 }
 </script>
 
 <style lang="scss">
+@import "./assets/styles/style.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/FileHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+    name: "FileHeader",
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

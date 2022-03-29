d02f6166d94e43efe9103ae2772c946b6ab1c195
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-29 23:36:14 +0200

    Added flag for language

--- src/components/FileFilmCard.vue
+++ src/components/FileFilmCard.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="col-3 gy-5">
-    <div class="film_card p-2">
+    <div class="film_card p-2 bg-danger">
       <p>Titolo: {{ objMovie.title }}</p>
       <p>Titolo originale: {{ objMovie.original_title }}</p>
-      <p>Lingua originale: {{ objMovie.original_language }}</p>
+      <p>Lingua originale: <lang-flag :iso="objMovie.original_language" :squared="false" /></p>
       <p>Voto: {{ objMovie.vote_average }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import LangFlag from 'vue-lang-code-flags';
 export default {
     name: "FileFilmCard",
+    components: {
+      LangFlag
+    },
     props: {
       objMovie: Object
     }
@@ -21,6 +25,5 @@ export default {
 <style lang="scss" scoped>
 .film_card{
   height: 400px;
-  background-color: red;
 }
 </style>

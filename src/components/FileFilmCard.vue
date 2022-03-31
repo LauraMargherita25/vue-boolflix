816bf008764fe24f521ebfff920b8d67a2395fa7
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-31 17:18:05 +0200

    Better animation

--- src/components/FileFilmCard.vue
+++ src/components/FileFilmCard.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="col-3 gy-5">
-    <div
-    class="film_card p-2" 
-    :style="{
-      'background-image': `url(https://image.tmdb.org/t/p/w342${objCard.img})`,
-      'background-size': 'cover',
-      'background-position': 'center',
-      'background-repeat': 'no-repeat',
-      }">
-      <div class="info_wrapper">
-        <p>Titolo: {{ objCard.title }}</p>
-        <p>Titolo originale: {{ objCard.originalTitle }}</p>
-        <p>Lingua originale: <lang-flag :iso="objCard.lang" :squared="false" /></p>
-        <ul class="d-flex align-items-baseline p-0">
-          <p>Voto:</p>
-          <li v-for="star in getRating(objCard.rating)" :key="star" class="text-warning p-1"><i class="fa-solid fa-star"></i></li>
-        </ul>
+    <div class="item_card">
+      <div class="item_card-wrapper">
+        <div class="card-front">
+          <img :src="'https://image.tmdb.org/t/p/w342' + objCard.img" alt="Avatar" style="width:100%;height:100%;">
+        </div>
+        <div class="card-back">
+          <div class="p-2">
+            <p>Titolo: {{ objCard.title }}</p>
+            <p>Titolo originale: {{ objCard.originalTitle }}</p>
+            <p>Lingua originale: <lang-flag :iso="objCard.lang" :squared="false" /></p>
+            <ul class="d-flex align-items-baseline p-0">
+              <p>Voto:</p>
+              <li v-for="star in getRating(objCard.rating)" :key="star" class="text-warning p-1"><i class="fa-solid fa-star"></i></li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </div> 
   </div>
 </template>
 
 <script>
 import LangFlag from 'vue-lang-code-flags';
+
 export default {
   name: "FileFilmCard",
   components: {
@@ -40,47 +41,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.film_card{
+.item_card {
+  background-color: transparent;
+  width: 300px;
   height: 400px;
-  background-color: red;
-  .info_wrapper{
-    display: none;
-  }
-  &:hover {
-    background-color: aqua;
-
-    animation: flip-in-ver-right 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-    .info_wrapper{
-      display: block;
+  perspective: 1000px; 
+  .item_card-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+    .card-front, .card-back {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      backface-visibility: hidden;
+    }
+    .card-back {
+      background-color: red;
+      color: white;
+      transform: rotateY(180deg);
     }
   }
-
-
-}
-@keyframes flip-in-ver-right {
-  0% {
-    transform: rotateY(-180deg);
-    opacity: 0;
+  &:hover .item_card-wrapper {
+    transform: rotateY(180deg);
   }
-  100% {
-    transform: rotateY(0);
-    opacity: 1;
-  }
-
-  // .info_wrapper{
-  //     display: none;
-  //   }
-  // &:hover{
-  //   background-color: aqua;
-  //   cursor: pointer;
-  //   animation: rotate 3s;
-  //   .info_wrapper{
-  //     display: block;
-  //   }
-  // }
-  // @keyframes rotate{
-  //   0%   {transform: rotateY(0deg);}
-  //   100% {transform: rotateY(180deg);}
-  // }
 }
 </style>

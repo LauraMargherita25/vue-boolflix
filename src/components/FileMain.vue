44a439ca9231aac6b571d38345e7faaa32e600a7
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-31 17:37:11 +0200

    No results no show

--- src/components/FileMain.vue
+++ src/components/FileMain.vue
@@ -1,11 +1,11 @@
 <template>
   <main>
     <div class="container">
-      <div class="row">
+      <div class="row" v-show="arrMovies.length !== 0">
         <h2 class="text-light">Film</h2>
         <file-film-card v-for="movie in arrMovies" :key="movie.title" :objCard="movie"/>
       </div>
-      <div class="row">
+      <div class="row mt-5" v-show="arrSeries.length !== 0">
         <h2 class="text-light">Serie tv</h2>
         <file-film-card v-for="serie in arrSeries" :key="serie.title" :objCard="serie"/>
       </div>
